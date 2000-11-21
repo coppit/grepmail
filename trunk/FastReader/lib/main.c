1eb1d098e16d6c7ba8e56b1e0487cf62c7b8869d
coppit <coppit@f7ece50a-8a89-11de-b841-6521bc8c149b>
2000-11-21 16:02:11 +0000

    Updated main.c to handle multiple input files. Modified fastreader.c to be a little faster, and to migrate toward chunk-based reading instead of line-based reading.

--- trunk/FastReader/lib/main.c
+++ trunk/FastReader/lib/main.c
@@ -5,31 +5,34 @@ extern int read_email(char **email,long *email_line);
 
 int main(int argc, char**argv)
 {
+  int i;
   FILE *infile;
   char *email;
+  char testfile[] = "../t/mailarc-1.txt";
   long line_number;
 
-  if (!(infile = fopen(argv[1], "r")))
+  if (argc == 1)
   {
-    fprintf (stderr, "can't open %s\n", argv[1]);
-    return 1;
+    argv[1] = testfile;
+    argc = 2;
   }
-/*
-  if (!(infile = fopen("../t/mailarc-1.txt", "r")))
-  {
-    fprintf (stderr, "can't open ../t/mailarc-1.txt\n", argv[1]);
-    return 1;
-  }
-*/
 
-  /* Only need this if we're using multiple files and we don't read until the
-   * end of each file.
-   * */
-  reset_file(infile);
-
-  while (read_email(&email,&line_number)) 
+  for (i=1;i<argc;i++)
   {
-    printf("########\nLINE: %d\n%s########\n",line_number,email);
+    if (!(infile = fopen(argv[i], "r")))
+    {
+      fprintf (stderr, "can't open %s\n", argv[i]);
+      return 1;
+    }
+
+    reset_file(infile);
+
+    while (read_email(&email,&line_number)) 
+    {
+      printf("########\nLINE: %d\n%s########\n",line_number,email);
+    }
+
+    fclose(infile);
   }
 
   return 0;
