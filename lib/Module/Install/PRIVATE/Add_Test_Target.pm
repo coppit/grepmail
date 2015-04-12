b6a8ab23df9426bc7f7bbba124d393e5cf7e8782
David Coppit <david@coppit.org>
2015-04-12 16:25:58 -0400

    Move testspeed Makefile editing to a Module::Install module

---
+++ lib/Module/Install/PRIVATE/Add_Test_Target.pm
@@ -0,0 +1,29 @@
+package Module::Install::PRIVATE::Add_Test_Target;
+
+use strict;
+use File::Slurp;
+
+use vars qw( @ISA $VERSION );
+
+use Module::Install::Base;
+@ISA = qw( Module::Install::Base );
+
+$VERSION = sprintf "%d.%02d%02d", q/0.10.0/ =~ /(\d+)/g;
+
+# ---------------------------------------------------------------------------
+
+sub Add_Test_Target
+{
+  my $self = shift;
+  my $target = shift;
+  my $test = shift;
+
+  *main::MY::postamble = sub {
+    return &Module::AutoInstall::postamble . <<EOF;
+$target :: pure_all
+  PERL_DL_NONLAZY=1 \$(PERLRUN) "-I\$(INST_LIB)" "-I\$(INST_ARCHLIB)" $test
+EOF
+  };
+}
+
+1;
